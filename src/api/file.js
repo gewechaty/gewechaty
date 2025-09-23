7f5bd4450a936a7d86763334b900009b9e75e511
gewechaty <452753617@qq.com>
2025-09-23 23:02:09 +0800

    处理图片下载接口2个最大并发，添加了图片下载队列

--- src/api/file.js
+++ src/api/file.js
@@ -1,6 +1,34 @@
-import {post} from '@/request/request'
+import { post } from '@/request/request'
 
-export const downloadFile = (data, config) => {
-  return post('/message/downloadImage', data, config)
+// 并发上限
+const MAX_CONCURRENCY = 2
+let active = 0
+const queue = []  // 存放待执行的任务（函数）
+
+function drainQueue() {
+  while (active < MAX_CONCURRENCY && queue.length) {
+    const task = queue.shift()
+    task && task()
+  }
 }
 
+export const downloadFile = (data, config) => {
+  return new Promise((resolve, reject) => {
+    const run = () => {
+      active++
+      post('/message/downloadImage', data, config)
+        .then(resolve)
+        .catch(reject)
+        .finally(() => {
+          active--
+          drainQueue()
+        })
+    }
+
+    if (active < MAX_CONCURRENCY) {
+      run()
+    } else {
+      queue.push(run)
+    }
+  })
+}
